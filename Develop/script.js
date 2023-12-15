573e702cd89c4dc0c03530bf96f1e34f7b8e7e13
SimpleSuyash <suyashmaharjan@gmail.com>
2023-12-15 20:28:27 +1100

    All functionlities added

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,20 @@
+dayjs.extend(window.dayjs_plugin_advancedFormat);
+
+
+
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function () {
- 
-  dayjs.extend(window.dayjs_plugin_advancedFormat);
 
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-  let schedules = [];
   let containerEl = $('#container');
-  let now = dayjs();
   let currentHour = dayjs().hour();
-  $('#currentDay').text(dayjs(now).format('dddd, MMMM Do'));
+  $('#currentDay').text(dayjs().format('dddd, MMMM Do'));
+  // let now = dayjs().startOf('h');
+  
 
-
-
-  for (let i = 1; i < 12; i++) {
+  for (let i = 9; i < 18; i++) {
 
     let timeBlockEl = $('<div>');
     let hourEl = $('<div>');
@@ -64,6 +44,7 @@ $(document).ready(function () {
     }
 
     timeBlockEl.attr('id', i);
+    timeBlockEl.attr('data-hour', hour);
 
 
     hourEl.addClass('col-2 col-md-1 hour text-center py-3');
@@ -82,42 +63,105 @@ $(document).ready(function () {
     saveIcon.attr('aria-hidden', 'true');
 
     saveEl.append(saveIcon);
-    timeBlockEl.append(hourEl);
-    timeBlockEl.append(scheduleEl);
-    timeBlockEl.append(saveEl);
-
+    timeBlockEl.append(hourEl, scheduleEl, saveEl);
     containerEl.append(timeBlockEl);
 
-  }
+    containerEl.on('click', '.saveBtn', handleSave);
 
-  containerEl.on('click', '.saveBtn', function (theEvent) {
-    let buttonPressedEl = $(theEvent.target.closest('button'));
-    let scheduleEl = buttonPressedEl.prev();
-    let taskBarEl= buttonPressedEl.parent();
-    let taskTimeEl = taskBarEl.children().first();
-    let task = scheduleEl.val().trim();
-    
-    if (task) {
-      let taskScheduled = {
-        'time': taskTimeEl.text(),
-        'task': scheduleEl.val().trim(),
-      };
-      schedules.push(taskScheduled);
-      scheduleEl.val("");
-      saveSchedule();
-    } else {
-      scheduleEl.val('');
-      $(scheduleEl).attr('placeholder', '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Your task is empty. The Schedule could not be saved. ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-      setTimeout(() => scheduleEl.attr('placeholder', ''), 3000);
-    }
-    
-  });
-  
-
-  function saveSchedule(){
-      localStorage.setItem('schedule', JSON.stringify(schedules));
   }
+  init();
+
+
+
+
 
 
 
 });//end of document.ready
+
+// This function is being called below and will run when the page loads.
+function init() {
+  // Get stored todos from localStorage
+  let storedSchedules = readSchedulesFromStorage();
+
+  // This is a helper function that will render todos to the DOM
+  renderSchedules(storedSchedules);
+}
+
+function renderSchedules(schedules) {
+
+
+  // Render a new li for each todo
+  for (let i = 0; i < schedules.length; i++) {
+    let sechedule = schedules[i];
+    let time = sechedule.time;
+    let task = sechedule.task;
+   
+    let taskEl = $('.row[data-hour =' + time + ']');
+    let scheduleEl = taskEl.children("textarea");
+    scheduleEl.val(task);
+    
+  }
+}
+// Reads projects from local storage and returns array of project objects.
+// Returns an empty array ([]) if there aren't any projects.
+function readSchedulesFromStorage() {
+  let schedules = localStorage.getItem('schedules');
+  if (schedules) {
+    schedules = JSON.parse(localStorage.getItem("schedules"));
+  } else {
+    schedules = [];
+  }
+  return schedules;
+}
+
+function handleSave(theEvent) {
+  let schedules = readSchedulesFromStorage();
+  let buttonPressedEl = $(theEvent.target.closest('button'));
+  let scheduleEl = buttonPressedEl.prev();
+  let taskBarEl = buttonPressedEl.parent();
+  let taskTimeEl = taskBarEl.children().first();
+  let task = scheduleEl.val().trim();
+  let scheduleExistAlready = false;
+  if (task) {
+    let scheduledTask = {
+      'time': taskTimeEl.text(),
+      'task': task,
+    };
+    schedules.forEach(function(schedule){
+      
+      if(schedule.time==scheduledTask.time){
+        schedule.task = scheduledTask.task;
+        scheduleExistAlready = true;
+      }
+    });
+    if(!scheduleExistAlready){
+      schedules.push(scheduledTask);
+    }
+    saveSchedulesToStorage(schedules);
+    let storedSchedules = readSchedulesFromStorage();
+    renderSchedules(storeSchedules);
+    scheduleEl.val("");
+    
+  } else {
+    scheduleEl.val('');
+    $(scheduleEl).attr('placeholder', '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Your task is empty. The Schedule could not be saved. ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+    setTimeout(() => scheduleEl.attr('placeholder', ''), 3000);
+  }
+
+
+};
+
+// Takes an array of projects and saves them in localStorage.
+function saveSchedulesToStorage(schedules) {
+    localStorage.setItem('schedules', JSON.stringify(schedules));
+    let saveMessageEl = $('#save-result');
+    saveMessageEl.append('Appontment Added to ', '<span>localstorage </span><i class="fa-solid fa-check fa-beat fa-xl"></i>');
+    setTimeout(() => saveMessageEl.html(''),3000);
+  }
+
+  
+
+
+
+  
